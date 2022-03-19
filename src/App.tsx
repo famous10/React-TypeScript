cc3260290d2879984dea8ce6fcd1de03a79a29a9
OSIFO  FAMOUS <70143479+famous10@users.noreply.github.com>
2022-03-19 20:27:13 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './App.css';
+import AddToList from './components/AddToList';
+import List from './components/List'
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
+
+ 
+
+function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "world Famous",
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      age: 36,
+      note: "Allegeric to staying on the same team"
+    }
+  ])
+
+  // people.map(person => {
+  //   person.name
+  // })
+
+
+  return (
+    <div className="App"> 
+      <h1>People Invited to my Game</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople}/>
+    </div>
+  );
+}
+
+export default App;
